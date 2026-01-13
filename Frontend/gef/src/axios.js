0351f27f80714a86d09cb19572fa1429efae4369
Unax Iriondo <unax.iriondo@ikasle.egibide.org>
2026-01-13 10:01:56 +0100

    formularios de crear empresa, usuario y ra

--- Frontend/gef/src/axios.js
+++ Frontend/gef/src/axios.js
@@ -4,7 +4,6 @@ axios.defaults.baseURL = 'http://localhost:8000/api'
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
-// Agregar token a las peticiones
 const token = localStorage.getItem('token')
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
